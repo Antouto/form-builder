c0247f824044cbceec7bc3bcd2385bc70335f686
Antouto <anthonydsmart@gmail.com>
2024-10-06 17:10:52 +0200

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -786,11 +786,11 @@ export function Editor({
 
                 let guildResponse = await getGuild(guild.id)
 
-                if (guildResponse) {
+                if (guildResponse && false) {
                   //@ts-expect-error
                   setCurrentGuild(guildResponse)
                 } else {
-                  const popup = window.open(`https://discord.com/oauth2/authorize?client_id=942858850850205717&permissions=378762431504&integration_type=0&scope=bot+applications.commands&guild_id=${guild.id}&disable_guild_select=true&response_type=code&redirect_uri=https%3A%2F%2Fform-builder.pages.dev%2Fapi%2Fdiscord%2Fauthorized`, 'popup', 'popup=true,width=485,height=675')
+                  const popup = window.open(`https://discord.com/oauth2/authorize?client_id=942858850850205717&permissions=378762431504&integration_type=0&scope=bot+applications.commands&guild_id=${guild.id}&disable_guild_select=true&response_type=code&redirect_uri=https%3A%2F%2Fform-builder.pages.dev%2Fapi%2Fdiscord%2Fauthorized`, 'popup', 'popup=true,width=485,height=700')
                   
                   window.addEventListener('message', (event) => {
                     if (event.data === 'authorized') {

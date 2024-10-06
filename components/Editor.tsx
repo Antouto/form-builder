bf74a1d70e97a630e0bd2eb98d68f912d7be45a9
Antouto <anthonydsmart@gmail.com>
2024-10-06 12:25:20 +0200

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -807,10 +807,10 @@ export function Editor({
             <Text fontSize={19} fontFamily='Whitney Bold'>Advanced</Text>
             <Button variant='secondary' onClick={() => setStage('editor')}>Open full editor</Button>
           </VStack>
-          <a href='https://discord.com/oauth2/authorize?client_id=942858850850205717&response_type=code&redirect_uri=https%3A%2F%2Fform-builder.pages.dev%2Fapi%2Fdiscord%2Fcallback&scope=identify+guilds&prompt=none'>
+          {/* <a href='https://discord.com/oauth2/authorize?client_id=942858850850205717&response_type=code&redirect_uri=https%3A%2F%2Fform-builder.pages.dev%2Fapi%2Fdiscord%2Fcallback&scope=identify+guilds&prompt=none'>
             <button style={{ color: 'darkgray' }}>{cookieValue ? `Cookie Value: ${cookieValue}` : '-'}</button>
           </a>
-          <button onClick={() => setStage('server_selection')} style={{ color: 'darkgray' }}>.</button>
+          <button onClick={() => setStage('server_selection')} style={{ color: 'darkgray' }}>.</button> */}
         </VStack></>}
         {stage === 'useCase' && <><Text mt={5} align='center' width='100%' fontSize={25} fontFamily='Whitney Bold'>What kind of form would you like to create?</Text>
           <VStack align='center' mt={10} width='100%' gap={10}>

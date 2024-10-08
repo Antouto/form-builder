e1aab0526d779fa9ff4da264cc9d02bd595bd6a3
Antouto <anthonydsmart@gmail.com>
2024-10-08 14:36:50 +0200

    Update SubmissionChannelIDInput.tsx

--- components/SubmissionChannelIDInput.tsx
+++ components/SubmissionChannelIDInput.tsx
@@ -8,10 +8,10 @@ export default function SubmissionChannelIDInput({ register, index, errors, fixM
   const [inputMethod, _setInputMethod] = useState() // login or manual
 
   function setInputMethod(method: string) {
-    
+
     //@ts-expect-error
     _setInputMethod(method)
-    if(method === 'login') {
+    if (method === 'login') {
       const popup = window.open(`https://discord.com/oauth2/authorize?client_id=942858850850205717&response_type=code&redirect_uri=https%3A%2F%2Fform-builder.pages.dev%2Fapi%2Fdiscord%2Fcallback&scope=identify+guilds&prompt=none`, 'popup', 'popup=true,width=485,height=700')
 
       window.addEventListener('message', (event) => {
@@ -48,27 +48,29 @@ export default function SubmissionChannelIDInput({ register, index, errors, fixM
 
       {/* Logged In or Input method manual */}
       {(cookieValue || (inputMethod === 'manual')) && (Array.isArray(currentGuild) ? <Select
-      height='36px!important'
-      // maxWidth='155px'
-      borderWidth='2px'
-      borderColor='transparent'
-      borderRadius='4px'
-      bg={colorMode === 'dark' ? 'grey.extradark' : 'grey.extralight'}
-      _focus={{ borderWidth: '2px', borderColor: 'blurple' }}
-      _hover={{ borderColor: 'transparent' }}
-      onChange={(event) => setValue(`forms.${index}.submit_channel_id`, event.target.value)}
-    // value={serverSubmissionMessage[index]}
-    >
-      <option disabled selected value="">Select a channel</option>
-      {currentGuild.filter(channel => ![2, 4, 13, 14].includes(channel.type)).map(channel => <option key={channel.id} value={channel.id}>{channel.name}</option>)}
-    </Select> : <>
-      <input
-        {...register(`forms.${index}.submit_channel_id`, { required: true, pattern: /^\d{10,20}$/, onChange: () => fixMessage() })}
-        id={`forms[${index}].submit_channel_id`}
-        style={{ marginBottom: '2px' }}
-      />
-      <button onClick={() => setInputMethod('login')} style={{ color: 'oklab(0.700834 -0.0780351 -0.1469)', fontSize: '15px' }}>Login to choose channel</button>
-    </>)}
+        height='36px!important'
+        // maxWidth='155px'
+        borderWidth='2px'
+        borderColor='transparent'
+        borderRadius='4px'
+        bg={colorMode === 'dark' ? 'grey.extradark' : 'grey.extralight'}
+        _focus={{ borderWidth: '2px', borderColor: 'blurple' }}
+        _hover={{ borderColor: 'transparent' }}
+        onChange={(event) => setValue(`forms.${index}.submit_channel_id`, event.target.value)}
+      // value={serverSubmissionMessage[index]}
+      >
+        <option disabled selected value="">Select a channel</option>
+        {currentGuild.filter(channel => ![2, 4, 13, 14].includes(channel.type)).map(channel => <option key={channel.id} value={channel.id}>{channel.name}</option>)}
+      </Select> : <>
+        <input
+          {...register(`forms.${index}.submit_channel_id`, { required: true, pattern: /^\d{10,20}$/, onChange: () => fixMessage() })}
+          id={`forms[${index}].submit_channel_id`}
+          style={{ marginBottom: '2px' }}
+        />
+        <ErrorMessage error={errors.forms?.[index]?.submit_channel_id || (onOpenWhereDoIFindSubmissionChannelID && !watch(`forms.${index}.submit_channel_id`) && { type: 'required' })} />
+        {!Array.isArray(currentGuild) && !onOpenWhereDoIFindSubmissionChannelID && <Text fontSize={12}>User Settings –&gt; Advanced –&gt; Enable Developer Mode<br /> Then go to the Submission Channel –&gt; Right Click –&gt; Copy Channel ID<br /><br /></Text>}
+        <button onClick={() => setInputMethod('login')} style={{ color: 'oklab(0.700834 -0.0780351 -0.1469)', fontSize: '15px' }}>Login to choose channel</button>
+      </>)}
 
 
 

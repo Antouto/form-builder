aec82be41a8850eea4f762ed2477dd058f659cff
Antouto <anthonydsmart@gmail.com>
2025-01-06 15:25:44 +0100

    Update [guild].js

--- functions/api/guilds/[guild].js
+++ functions/api/guilds/[guild].js
@@ -2,7 +2,9 @@ export async function onRequest({ params }) {
   const guildId = params.guild
 
   const guildResponse = await fetch(
-    `https://create.discordforms.app/api/discord/session?guild_id=${guildId}&checkIfAdmin=true`
+    `https://create.discordforms.app/api/discord/session?guild_id=${guildId}&checkIfAdmin=true`, {
+      credentials: "same-origin"
+    }
   ).then(res => res.text());
 
   return new Response(guildResponse)
